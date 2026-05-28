81b14fe1e7a5d09583cfe5460255b929253a28b0
{Flo2100049} <{gianiskamai@gmail.com}>
2026-05-28 05:31:53 -0400

    Fixes

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -93,8 +94,6 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
-
-
 
 
 ######################################################################
@@ -136,7 +135,6 @@ def delete_accounts(account_id):
         account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
-
 
 
 ######################################################################
